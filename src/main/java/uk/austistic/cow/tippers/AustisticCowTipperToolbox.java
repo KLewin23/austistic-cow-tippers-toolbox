3e87d3bbbc782ce1ee731fb0e2e7270c5ba1a6d3
Kieran Lewin <kieran.lewin2000@gmail.com>
2026-09-05 17:52:24 +0100

    add mod code

--- src/main/java/uk/austistic/cow/tippers/AustisticCowTipperToolbox.java
+++ src/main/java/uk/austistic/cow/tippers/AustisticCowTipperToolbox.java
@@ -7,6 +7,7 @@ import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
+import uk.austistic.cow.tippers.londonundergroundtrains.Init
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(AustisticCowTipperToolbox.MODID)
@@ -19,5 +20,6 @@ public class AustisticCowTipperToolbox {
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public AustisticCowTipperToolbox(IEventBus modEventBus, ModContainer modContainer) {
+        londonundergroundtrains.Init.init()
     }
 }
